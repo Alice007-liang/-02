5709df274d009906c02965f745b227d6278023ff
Alice007-liang <lyw20181231@163.com>
2021-03-22 11:27:20 +0800

    课本第一、二章例题

---
+++ Exercise/src/tip11.java
@@ -0,0 +1,24 @@
+
+public class tip11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long totalMilliseconds = System.currentTimeMillis();
+		
+		long totalSeconds = totalMilliseconds / 1000;
+		
+		long currentSeconds = totalSeconds % 60;
+		
+		long totalMinutes = totalSeconds / 60;
+		
+		long currentMinute = totalSeconds % 60;
+		
+		long totalHours = totalMinutes / 60;
+		
+		long currentHour = totalHours % 24;
+		
+		System.out.println("Current time is " + currentHour + ":" 
+		+ currentMinute + ":" + currentSeconds + "GMT");
+	}
+
+}
